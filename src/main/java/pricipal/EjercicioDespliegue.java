2189bc891183d16db10b6a4513e3047ff715d061
machimicu <machimicu@Enrique-TM1703>
2019-12-02 19:45:22 +0100

    He añadido otro sout

--- src/main/java/pricipal/EjercicioDespliegue.java
+++ src/main/java/pricipal/EjercicioDespliegue.java
@@ -9,6 +9,7 @@ public class EjercicioDespliegue {
 
     public static void main(String[] args) {
         System.out.println("Ejercicio despliegue");
+        System.out.println("Cambio XD");
     }
     
 }
